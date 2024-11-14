c521eeb05cf26eaf245e15a60913b6e4f182a4da
Leo Sundberg <leo.sundberg@elev.ga.ntig.se>
2024-11-14 10:22:29 +0100

    Övning 3 klar

--- Övning3/Program.cs
+++ Övning3/Program.cs
@@ -1,2 +1,64 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void Hello32()
+{
+    for (int i = 0; i < 32; i++)
+    {
+        Console.WriteLine("Hello world");
+    }
+}
+int Squared(int f)
+{
+    return f*f;
+}
+float Multi(float f1, float f2)
+{
+    return f1 * f2;
+}
+float RightTriangleArea(float f1, float f2)
+{
+    return f1 * f2 / 2;
+}
+double CircleArea(float f1)
+{
+    return f1 * f1 * 3.14159265;
+}
+int GetNumberInput()
+{
+    string numInput = "";
+    int numOutput = 0;
+    bool success = false;
+    while (!success)
+    {
+        numInput = Console.ReadLine();
+        success = int.TryParse(numInput, out numOutput);
+    }
+    return numOutput;
+}
+
+int GetChoice(string[] a)
+{
+    bool success = false;
+    string numInput = "";
+    int numOutput = 0;
+    // string choice = "";
+    for (int i = 0; i < a.Length; i++)
+    {
+        Console.WriteLine(a[i]);
+    }
+    while (!success)
+    {
+        Console.WriteLine($"Pick between option 0 and option {a.Length - 1}!");
+        numInput = Console.ReadLine();
+        success = int.TryParse(numInput, out numOutput);
+        if (numOutput < 0 || numOutput >= a.Length)
+        {
+            success = false;
+        }
+
+    }
+    Console.WriteLine(numInput);
+    return numOutput;
+}
+
+GetChoice(["balls", "mammals", "gyatt", "rizzler", "ohio"]);
+
+Console.ReadLine();
--- Övning3/obj/Debug/net8.0/Övning3.AssemblyInfo.cs
+++ Övning3/obj/Debug/net8.0/Övning3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Övning3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2c08d2c1535c7790224926c2e802b7eaa508665")]
 [assembly: System.Reflection.AssemblyProductAttribute("Övning3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Övning3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
